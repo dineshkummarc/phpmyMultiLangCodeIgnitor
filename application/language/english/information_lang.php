8b5c2dc72344913e3fafe199df8a9a2511dda085
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-21 23:38:20 +0530

    update

--- application/language/english/information_lang.php
+++ application/language/english/information_lang.php
@@ -2,27 +2,6 @@
 
 $lang['welcome'] = 'WELCOME';
 $lang['site_name'] = 'phpmyMultiLangCodeIgnitor';
-//$lang['assign_books'] = 'Assign Stock';
-//$lang['assign_new'] = 'Assign New';
-//$lang['issued'] = 'Issued';
-//$lang['members'] = 'Members';
-//$lang['add_members'] = 'Add Members';
-//$lang['all_members'] = 'All Members';
-//$lang['books'] = 'Borrow Stocks';
-//$lang['stock'] = 'Stocks';
-//$lang['all_books'] = 'All Stock';
-//$lang['add_books'] = 'Add Stock';
-//$lang['all_borrowed'] = 'All Stocks';
-//$lang['add_borrow'] = 'Borrow Stocks';
-//$lang['publishers'] = 'Publishers';
-//$lang['add_publishers'] = 'Add Publisher';
-//$lang['all_publishers'] = 'All Publisher';
-//$lang['author'] = 'Author';
-//$lang['add_author'] = 'Add Author';
-//$lang['all_author'] = 'All Authors';
-//$lang['subject'] = 'Subject';
-//$lang['all_subject'] = 'All Subjects';
-//$lang['add_subject'] = 'Add Subject';
 $lang['change_language'] = 'Change Language';
 $lang['english'] = 'English';
 $lang['urdu'] = 'Urdu';
