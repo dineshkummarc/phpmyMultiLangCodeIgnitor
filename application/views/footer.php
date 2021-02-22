7fb0ddc7fc534429f49c5eb638ede6b26043f9f5
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-22 22:56:37 +0530

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,9 @@
-<p class='footer'>footer check</p>
+<p class='footer'><?php echo lang('copyright');?> 2021 <?php echo lang('author');?>. <?php echo lang('legal');?>.</p>
 	</div>
 	<script type="text/javascript">
-		
 		    $(document).ready(function () {
 				$('table').dataTable();
 			});
-		
 	</script>
 </body>
 </html>
