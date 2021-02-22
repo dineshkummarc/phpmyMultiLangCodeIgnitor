b7c384f20be64f8e9a251b3bfb0df1cedcc451cd
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-22 18:12:49 +0530

    update

--- application/language/urdu/information_lang.php
+++ application/language/urdu/information_lang.php
@@ -1,9 +1,14 @@
 <?php
 
 $lang['welcome'] = 'خوش آمدید';
-$lang['site_name'] = 'اہلیب';
+$lang['site_name'] = 'پی ایچ پییمی ملٹی لانگ کوڈ ایگینیٹر';
 $lang['change_language'] = 'زبان تبدیل کریں';
 $lang['english'] = 'انگریزی';
 $lang['urdu'] = 'اردو';
-
+$lang['hindi'] = 'ہندی';
+$lang['logout'] = 'لاگ آوٹ';
+$lang['to'] = 'کرنے کے لئے';
+$lang['login_now'] = 'ابھی لاگ ان کریں';
+$lang['email'] = 'ای میل';
+$lang['password'] = 'پاس ورڈ';
 ?>
