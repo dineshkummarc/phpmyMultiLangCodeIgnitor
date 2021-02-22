18f90b4da487ac94808ac0017faf3d872c0fad84
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-22 18:11:39 +0530

    update

--- application/language/english/information_lang.php
+++ application/language/english/information_lang.php
@@ -5,4 +5,10 @@ $lang['site_name'] = 'phpmyMultiLangCodeIgnitor';
 $lang['change_language'] = 'Change Language';
 $lang['english'] = 'English';
 $lang['urdu'] = 'Urdu';
+$lang['hindi'] = 'Hindi';
+$lang['logout'] = 'Logout';
+$lang['to'] = 'to';
+$lang['login_now'] = 'Login Now';
+$lang['email'] = 'Email';
+$lang['password'] = 'Password';
 ?>
