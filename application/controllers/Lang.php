8cb4efc72ca15fd823acd83e557f05b860e1fb50
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-22 20:09:17 +0530

    update

--- application/controllers/Lang.php
+++ application/controllers/Lang.php
@@ -4,15 +4,12 @@
 */
 class Lang extends CI_Controller
 {
-	
 	function __construct()
 	{
 		parent::__construct();
 		$this->lang->load('information','urdu');
-		
 	}
 	function index(){
-
 		echo $this->lang->line('welcome');
 	}
 }
